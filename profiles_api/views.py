9bbfd5735fa55db2ad64df9796095fd296560c19
lijuktra <lijuktra@yahoo.com>
2019-06-24 19:54:36 +0200

    update all json methods post, put , patch , get and delete in API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     """ Test API HelloApiView"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ returns the list of API fetaures """
@@ -14,3 +19,30 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """ create a message with the name"""
+        #retrive the data from request and validate and validate the data via serilizer
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message' : message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+    def put(self, request , pk=None):
+        """ Handle updating an object"""
+        return Response({'method' : 'PUT'})
+
+    def patch(self, request , pk=None):
+        """ Handle a partial update on an object"""
+        return Response({'method' : 'PATCH'})
+
+    def delete(self, request , pk=None):
+        """ Handle deleting an object"""
+        return Response({'method' : 'DELETE'})
