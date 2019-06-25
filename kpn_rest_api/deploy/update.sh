3d0fb5e22df6d9d535b09474e2b755bf4c53fb1f
lijuktra <lijuktra@yahoo.com>
2019-06-25 16:31:54 +0200

    new commit

--- kpn_rest_api/deploy/update.sh
+++ kpn_rest_api/deploy/update.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/django-kpn-rest-api-demo'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart kpn-rest-api
 
 echo "DONE! :)"
--- kpn_rest_api/urls.py
+++ kpn_rest_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from profiles_api import views
+from kpn-rest-api import views
 
 router = DefaultRouter()
 router.register('hello_viewset', views.HelloViewSet, base_name='hello-viewset')
