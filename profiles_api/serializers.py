ac213ef84d379724a4ebca6eaa507512139cec0a
lijuktra <lijuktra@yahoo.com>
2019-06-25 11:58:49 +0200

    Configured for deployment .

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """Test by Liju to test a name field for testing the APIView"""
     name = serializers.CharField(max_length=9)
+
+    
