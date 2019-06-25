2dec88d432fb8e1311312906cd664de821d29c21
lijuktra <lijuktra@yahoo.com>
2019-06-25 20:28:08 +0200

    add azure host name

---
+++ profiles_api-back/deploy/update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+
+git pull
+$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py collectstatic --noinput
+supervisorctl restart profiles_api
+
+echo "DONE! :)"
